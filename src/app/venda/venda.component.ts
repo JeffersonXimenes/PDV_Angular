c86f2f7216545f91dbf01b8728ff9311bb5d2059
Paulo Henrique Santos <phsantosferreira9@gmai.com>
2020-08-27 22:52:06 -0300

    Adicionando funcoes tela de venda

--- src/app/venda/venda.component.ts
+++ src/app/venda/venda.component.ts
@@ -1,4 +1,7 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, ViewChild } from '@angular/core';
+import { ProdutoService } from './shared/produto.service';
+import { ResponseProduto, Produto } from './shared/produto.model';
+import { NgForm } from '@angular/forms';
 
 @Component({
   selector: 'app-venda',
@@ -6,10 +9,38 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./venda.component.css']
 })
 export class VendaComponent implements OnInit {
+  
+  @ViewChild('formProdutos', { static: true }) formTarefa: NgForm;
 
-  constructor() { }
+  constructor(private produtoService: ProdutoService) { }
 
+  responseProduto: ResponseProduto[];
+  request: any;
+  listaDeProdutos: Array<VendaItem> = [];
+  id: number;
+  quantidade: number;
+  total: number = 0;
+  qtdTotal: number = 0;
+  desconto: number = 0;
   ngOnInit(): void {
   }
 
+  buscarProduto() {
+    this.produtoService.getProduto(this.id).subscribe(response => {this.request = response; this.addProdutoLista();console.log(this.request.cdProduto);
+    });
+  }
+
+  addProdutoLista() {
+    let item = new VendaItem(this.request.cdProduto, this.request.valorProduto, this.request.descricaoProduto, this.quantidade);
+    this.listaDeProdutos.push(item);
+    console.log(this.listaDeProdutos);
+    this.total += (this.request.valorProduto * this.quantidade);
+    this.qtdTotal += (this.quantidade);
+
+  }
+}
+
+class VendaItem {
+  constructor(public cdProduto: number, public valorProduto: number, public descricaoProduto: String, 
+    public qtdProduto: number) { }
 }
